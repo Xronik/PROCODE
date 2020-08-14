989aa7a8d199cdde4445ade78f64037e7303cdd6
Andrii <xronik2000@gmail.com>
2020-08-14 19:17:19 +0300

    added article and button

--- 07.08.20/js/main.js
+++ 07.08.20/js/main.js
@@ -1,1 +1,9 @@
-let start = document.getElementById('start');
+let btn = document.getElementsByClassName('start');
+var second = document.getElementsByClassName('second');
+console.log(btn);
+console.log(second);
+// function test() {
+//     return second.style.width = "300px";
+// }
+
+// test.onclick();  
