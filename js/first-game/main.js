53880af9d9bb0754cf06c48fb059999201ee69df
Xronik <xronik2000@gmail.com>
2020-10-10 14:36:05 +0300

    add first game v5

--- js/first-game/main.js
+++ js/first-game/main.js
@@ -19,7 +19,7 @@ let count = (array) => { // Подсчитываем суму всех не ну
     }
     return sum
 }
-
+let btn = document.querySelectorAll('.btn')
 let main = document.querySelector('.main')
 let statusField = document.querySelector('.status')
 let currentPosition = []
@@ -57,47 +57,74 @@ newBoard[0][0] = 1
 boardDisplay(newBoard)
 let cells = document.getElementsByClassName('cell')
 
-let moveRight = () => {  // Движение вправо
-    path(newBoard)
-    if (currentPosition[1] < newBoard.length - 1) {
-        currentPosition[1]++
-        if (newBoard[currentPosition[0]][currentPosition[1]] === 2) { currentPosition[1]-- } else { true };
-        newBoard[currentPosition[0]][currentPosition[1]] = 1
-        boardDisplay(newBoard)
-    }
-}
-let moveLeft = (event) => {  // Движение влево
+let moveLeft = () => {  // Движение влево
     path(newBoard)
     if (currentPosition[1] > 0) {
         currentPosition[1]--
-        if (newBoard[currentPosition[0]][currentPosition[1]] === 2) { currentPosition[1]++ } else { true };
+        if (newBoard[currentPosition[0]][currentPosition[1]] === 2) {
+            currentPosition[1]++
+            btnOff(btn[0])
+        } else {
+            btnOn(btn[0], '&#8592;')
+        };
         newBoard[currentPosition[0]][currentPosition[1]] = 1
         boardDisplay(newBoard)
-    }
+    } else { btnOff(btn[0]) }
 }
 let moveUp = () => {  // Движение вверх
     path(newBoard)
     if (currentPosition[0] > 0) {
         currentPosition[0]--
-        if (newBoard[currentPosition[0]][currentPosition[1]] === 2) { currentPosition[0]++ } else { true };
+        if (newBoard[currentPosition[0]][currentPosition[1]] === 2) {
+            currentPosition[0]++
+            btnOff(btn[1])
+        } else {
+            btnOn(btn[1], '&#8593;')
+        };
         newBoard[currentPosition[0]][currentPosition[1]] = 1
         boardDisplay(newBoard)
-    }
+    } else { btnOff(btn[1]) }
 }
 let moveDown = () => {  // Движение вниз
     path(newBoard)
     if (currentPosition[0] < newBoard.length - 1) {
         currentPosition[0]++
-        if (newBoard[currentPosition[0]][currentPosition[1]] === 2) { currentPosition[0]-- } else { true };
+        if (newBoard[currentPosition[0]][currentPosition[1]] === 2) {
+            currentPosition[0]--
+            btnOff(btn[2])
+        } else {
+            btnOn(btn[2], '&#8595;')
+        };
         newBoard[currentPosition[0]][currentPosition[1]] = 1
         boardDisplay(newBoard)
-    }
+    } else { btnOff(btn[2]) }
+}
+let moveRight = () => {  // Движение вправо
+    path(newBoard)
+    if (currentPosition[1] < newBoard.length - 1) {
+        currentPosition[1]++
+        if (newBoard[currentPosition[0]][currentPosition[1]] === 2) {
+            currentPosition[1]--
+            btnOff(btn[3])
+        } else {
+            btnOn(btn[3], '&#8594;')
+        };
+        newBoard[currentPosition[0]][currentPosition[1]] = 1
+        boardDisplay(newBoard)
+    } else { btnOff(btn[3]) }
 }
 let path = (array) => { // Рисуем путь
     array[currentPosition[0]][currentPosition[1]] = 2
 }
+let btnOff = (btn) => {
+    btn.style.userSelect = 'none'
+    btn.innerHTML = ''
+}
+let btnOn = (btn, value) => {
+    btn.style.userSelect = 'auto'
+    btn.innerHTML = `${value}`
+}
 
-let btn = document.querySelectorAll('.btn')
 btn[0].addEventListener("mousedown", moveLeft);
 btn[1].addEventListener("mousedown", moveUp);
 btn[2].addEventListener("mousedown", moveDown);
